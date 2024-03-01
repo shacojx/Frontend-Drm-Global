c831562bf2067420527091c9b842b18ec17cbf5f
hoangf <hoangf.nv@gmail.com>
2024-03-02 02:14:53 +0700

    update UI

--- web-app/src/layouts/PageLayoutOneForm.tsx
+++ web-app/src/layouts/PageLayoutOneForm.tsx
@@ -5,7 +5,7 @@ import { FooterDefault } from "../components/base/footers";
 
 export function PageLayoutOneForm(props: PropsWithChildren<{}>) {
   return <div className="w-screen h-screen p-4 bg-cover flex flex-col" style={{backgroundImage: `url(${background})`}}>
-    <div className="flex flex-col items-center justify-center grow">
+    <div className="flex flex-col items-center justify-center grow my-4">
       <div className="w-full max-w-[400px]">
         <img className="mb-8" src={logo_full} alt="logo_full"/>
         <div className="flex flex-col gap-y-8 py-12 px-8 shadow-form rounded-3xl">
