7d347f1246d0f91a83587b9edfb4dbfccf556a4a
hoangf <hoangf.nv@gmail.com>
2024-03-21 01:51:54 +0700

    add no conflict file

--- web-app/src/components/FormFieldArea.tsx
+++ web-app/src/components/FormFieldArea.tsx
@@ -2,36 +2,46 @@ import React, { ChangeEvent } from "react";
 import { useTranslation } from "react-i18next";
 import { useValidate } from "../hooks-ui/useValidateCaller";
 import { FormFieldProps } from "../types/common";
+import clsx from "clsx";
 
 export function FormFieldTextArea(props: FormFieldProps<string>) {
-  const translation = useTranslation()
+  const translation = useTranslation();
   const [shouldShowError, setShouldShowError] = useValidate(
     props.id,
     props.isRequired,
     props.value,
     props.validateCaller
-  )
+  );
 
   function handleChange(event: ChangeEvent<HTMLTextAreaElement>) {
-    const value = event.target.value
-    props.onChange(value)
+    const value = event.target.value;
+    props.onChange(value);
   }
 
-  const isTextValid = !props.isRequired || !!props.value
-  const statusClassName = shouldShowError ? 'border-danger bg-red-50' : 'bg-white'
-  return <div className="flex flex-col gap-2">
-    {!!props.label &&
-      <p className="flex text-cBase font-bold gap-1">
-        <span>{translation.t(props.label)}</span>
-        {props.isRequired && <span className="text-danger">*</span>}
-      </p>
-    }
-    <textarea
-      value={props.value || ''}
-      onChange={handleChange}
-      onBlur={setShouldShowError.bind(undefined, !isTextValid)}
-      placeholder={props.placeholder}
-      className={"w-full min-h-[40px] border py-1 px-2 rounded-lg " + statusClassName}
-    />
-  </div>
+  const isTextValid = !props.isRequired || !!props.value;
+  const statusClassName = shouldShowError
+    ? "border-danger bg-red-50"
+    : "bg-white";
+  return (
+    <div className="flex flex-col gap-2">
+      {!!props.label && (
+        <p className="flex text-cBase font-bold gap-1">
+          <span>{translation.t(props.label)}</span>
+          {props.isRequired && <span className="text-danger">*</span>}
+        </p>
+      )}
+      <textarea
+        value={props.value || ""}
+        onChange={handleChange}
+        onBlur={setShouldShowError.bind(undefined, !isTextValid)}
+        placeholder={props.placeholder}
+        // className={"w-full min-h-[40px] border py-1 px-2 rounded-lg " + statusClassName}
+        className={clsx(
+          "w-full min-h-[40px] py-1 px-2 rounded-lg",
+          statusClassName,
+          props.isFixedValue ? "border-none outline-none" : "border"
+        )}
+      />
+    </div>
+  );
 }
--- web-app/src/types/common.d.ts
+++ web-app/src/types/common.d.ts
@@ -1,23 +1,27 @@
 import React from "react";
 
-export type FormStatus = 'typing' | 'requesting' | 'success' | 'failure'
-type OptionInfo<T extends React.Key > = {
-  value: T,
-  label: string,
-  iconElement?: JSX.Element,
-}
-export type ValidateFunction<T> = (isRequired: FormFieldProps<T>['isRequired'], value: FormFieldProps<T>['value']) => boolean
-type ValidateHock = () => boolean
-type ValidateCaller = Record<string, ValidateHock>
+export type FormStatus = "typing" | "requesting" | "success" | "failure";
+type OptionInfo<T extends React.Key> = {
+  value: T;
+  label: string;
+  iconElement?: JSX.Element;
+};
+export type ValidateFunction<T> = (
+  isRequired: FormFieldProps<T>["isRequired"],
+  value: FormFieldProps<T>["value"]
+) => boolean;
+type ValidateHock = () => boolean;
+type ValidateCaller = Record<string, ValidateHock>;
 type FormFieldProps<T> = {
-  id: string,
-  validateCaller: ValidateCaller
-  onChange: (value: T) => void,
+  id: string;
+  validateCaller: ValidateCaller;
+  onChange: (value: T) => void;
 } & Partial<{
-  label: string,
-  isRequired: boolean,
-  placeholder: string,
-  value: T,
-  errorMessage: string,
-  isFixedValue: boolean
-}>
+  label: string;
+  isRequired: boolean;
+  placeholder: string;
+  value: T;
+  errorMessage: string;
+  isFixedValue: boolean;
+  className: string;
+}>;
