60898fa5b51e9ffd06ebda987a2674c1e07cdda3
luc.le <luc.le@enouvo.com>
2024-03-21 18:56:55 +0700

    implement upload api

--- web-app/src/api/upload.ts
+++ web-app/src/api/upload.ts
@@ -1,4 +1,4 @@
-import { getAccessTokenInfo, getAuthorizationString } from "src/services-base/api";
+import { callApi, getAccessTokenInfo, getAuthorizationString } from "src/services-base/api";
 import { UploadResponse } from "./types";
 
 export const uploadFile = async (file: File) => {
@@ -32,9 +32,18 @@ export const getFile = async (name: string) => {
     headers: headers,
   };
 
+  // const endpoint = `${process.env.REACT_APP_URL}/api/file/files/${name}`;
   const endpoint = `${process.env.REACT_APP_URL}/api/file/files/${name}`;
-  fetch(endpoint, options)
-    .then((response) => response.text())
-    .then((result) => console.log(result))
+
+  const blob = await fetch(endpoint, options)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const a = document.createElement("a");
+      const url = URL.createObjectURL(blob);
+      a.href = url;
+      a.download = name;
+      a.click();
+      URL.revokeObjectURL(url);
+    })
     .catch((error) => console.log("error", error));
 };
