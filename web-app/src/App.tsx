9d0ccbd29acf70fc3c7f350df10c3a3ee948fd35
Nguyen Van Hoang <hoang.mmj@gmail.com>
2024-03-22 13:58:30 +0700

    add tailwind-merge clsx react-query

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router-dom";
 import './App.css';
 import { AuthContextProvider } from './contexts/AuthContextProvider';
 import { router } from "./pages/router";
 
-export const queryClient = new QueryClient()
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: 0
+    }
+  },
+})
 
 function App() {
   return <QueryClientProvider client={queryClient}>
     <AuthContextProvider>
-      <RouterProvider router={router} />
+       <RouterProvider router={router} />
     </AuthContextProvider>
   </QueryClientProvider>
 }
