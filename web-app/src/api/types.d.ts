3db3520b52a891eab311b5599a208897b8e01898
luc.le <luc.le@enouvo.com>
2024-03-25 01:56:14 +0700

    fix format

--- web-app/src/api/types.d.ts
+++ web-app/src/api/types.d.ts
@@ -1,114 +1,108 @@
-import { ServiceType } from string;
+import { ServiceType } from "../pages/LLCMyService/types/my-service.type";
 
-export type NationValue = string;
-export type CompanyTypeValue = string;
-export type EntityEnding =
-  | string
-  | string
-  | string
-  | string
-  | string;
-export type Industry = string;
-export type NationPhone = string;
-export type LocalPhone = string;
+export type NationValue = string
+export type CompanyTypeValue = 'LLC' | 'PLC'
+export type EntityEnding = 'LLC' | 'L.L.C' | 'LIMITED LIABILITY COMPANY' | 'PRIVATE LIMITED COMPANY' | ''
+export type Industry = string
+export type NationPhone = string
+export type LocalPhone = string
 
-export type RawResultEmpty = string;
+export type RawResultEmpty = ''
 
 // ====== Account ======== //
 export type ApiLoginParam = {
-  username: string;
-  password: string;
-};
-export type UserRole = string;
+  username: string,
+  password: string,
+}
+export type UserRole = 'ROLE_USER'
 export type RawResultLogin = {
-  id: number;
-  email: string;
-  token: string;
-  refreshToken: string;
-  type: string;
-  username: string;
-  roles: UserRole[];
-};
-export type TransformedResultLogin = RawResultLogin;
+  id: number,
+  email: string,
+  token: string,
+  refreshToken: string,
+  type: string,
+  username: string,
+  roles: UserRole[],
+}
+export type TransformedResultLogin = RawResultLogin
 
 export type ApiSendRecoveryCode = {
-  email: string;
-};
-export type RawResultSendRecoveryCode = {};
+  email: string
+}
+export type RawResultSendRecoveryCode = {}
 
 export type ApiVerifyPhone = {
-  codePhone: NationPhone;
-  phone: LocalPhone;
-};
+  "codePhone": NationPhone,
+  "phone": LocalPhone,
+}
 
 export type ApiCheckRecoveryCode = {
-  email: string;
-  otp: string;
-};
-export type RawResultCheckRecoveryCode = string;
+  "email": string,
+  "otp": string,
+}
+export type RawResultCheckRecoveryCode = string
 
 export type ApiRegisterAccountParam = {
-  llcInNation: NationValue;
-  email: string;
-  phone: string;
-  codePhone: NationPhone;
-  companyType: CompanyTypeValue;
-  firstName: string;
-  lastName: string;
-  password: string;
-  rePassword: string;
-  otpVerifyEmail: string;
+  "llcInNation": NationValue,
+  "email": string,
+  "phone": string,
+  "codePhone": NationPhone,
+  "companyType": CompanyTypeValue,
+  firstName: string,
+  lastName: string,
+  "password": string,
+  "rePassword": string,
+  "otpVerifyEmail": string,
 } & Partial<{
-  companyName: string;
-  entityEnding: EntityEnding;
-  industry: Industry;
-  website: string;
-  companyDescription: string;
-}>;
-export type RawResultRegisterAccount = Omit<ApiRegisterAccountParam, string>;
+  "companyName": string,
+  "entityEnding": EntityEnding,
+  "industry": Industry,
+  "website": string,
+  "companyDescription": string,
+}>
+export type RawResultRegisterAccount = Omit<ApiRegisterAccountParam, 'password' | 'rePassword'>
 
 export type ApiResetPasswordParam = {
-  newPass: string;
-  reNewPass: string;
-  signature: string;
-};
-export type RawResultResetPassword = string;
+  newPass: string,
+  reNewPass: string,
+  signature: string,
+}
+export type RawResultResetPassword = ""
 
-export type KYCStatus = string;
+export type KYCStatus = 'Pending' | 'In-progress' | 'Approved'
 
 export type RawResultGetUserProfile = {
-  llcInNation: NationValue;
-  email: string;
-  avatar: string;
-  codePhone: NationPhone;
-  phone: string;
-  companyType: CompanyTypeValue;
-  firstName: string;
-  lastName: string;
-  kycStatus: KYCStatus;
-};
+  "llcInNation": NationValue,
+  "email": string,
+  "avatar": string,
+  "codePhone": NationPhone,
+  "phone": string,
+  "companyType": CompanyTypeValue,
+  firstName: string,
+  lastName: string,
+  kycStatus: KYCStatus,
+}
 
 export type ApiChangeUserProfile = {
-  email: string;
-  codePhone: NationPhone;
-  phone: string;
-  firstName: string;
-  lastName: string;
-};
+  "email": string,
+  "codePhone": NationPhone,
+  "phone": string,
+  "firstName": string,
+  "lastName": string,
+}
 export type ApiChangeUserPassword = {
-  newPass: string;
-  reNewPass: string;
-};
+  "newPass": string,
+  "reNewPass": string,
+}
 
 export type ApiUploadKYC = {
-  passport: File;
-  picture: File;
+  passport: File
+  picture: File
 };
 
 // ====== Payment ======== //
-
-export type Currency = string;
-export type OrderType = string;
+export type Currency = 'USD'
+export type OrderType = 'PAYPAL'
 
 export type ApiCreateOrderParam = {
   transactionId: string;
@@ -163,7 +157,7 @@ export type OwnerInformation = {
   companyName?: string;
   ownership: number; // INFO: (%)
   document: string[];
-  type: string;
+  type: "Company" | "Individual";
   firstName?: string;
   lastName?: string;
 };
