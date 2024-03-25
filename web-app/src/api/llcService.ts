71f6c6673b5bed9574dbb7422a7ad1c04089559c
Tran Van Hoan <tranhoan.dz98@gmail.com>
2024-03-25 09:45:52 +0700

    update type and fix trang thai

--- web-app/src/api/llcService.ts
+++ web-app/src/api/llcService.ts
@@ -10,26 +10,7 @@ const URL_UPLOAD = 'api/file/upload-customer-document'
 
 export async function callApiGetListLlcServices() {
   // TODO: implement call api
-  // sample data = [{
-// contractFile: null
-// createdAt: "2024-03-24T16:14:15.000+00:00"
-// cycleNumber: 1
-// id: 1
-// paymentMethod: null
-// pic: "drm001@gmail.com"
-// pricePerCycle: 1000
-// serviceDescription: "Dev create for dev"
-// serviceId: 1
-// serviceName: "Dev MS 1"
-// serviceStep: Array [ {…}, {…} ]
-// serviceType: "Based"
-// statusContract: "Pending"
-// statusPayment: "Pending"
-// statusService: "Pending"
-// transitionId: 1711296855259
-// updatedAt: "2024-03-24T19:32
-// }]
-  return await callApi<any>("GET", `${URL}`, {}, true);
+  return await callApi<MyServiceType[]>("GET", `${URL}`, {}, true);
 }
 
 // @ts-ignore
