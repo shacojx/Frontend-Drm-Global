624ec8b2d392d217c56e7e6fa25eb851f44bec45
luc.le <luc.le@enouvo.com>
2024-03-27 17:42:03 +0700

    setup font

--- landing/app/layout.tsx
+++ landing/app/layout.tsx
@@ -1,12 +1,43 @@
 import type { Metadata } from 'next';
-import { Open_Sans } from 'next/font/google';
+import localFont from 'next/font/local';
 import type { PropsWithChildren } from 'react';
 
 import '@/app/globals.css';
 
-const fontSans = Open_Sans({
+const fontSans = localFont({
   variable: '--font-sans',
-  subsets: ['latin'],
+  src: [
+    {
+      path: '../assets/fonts/GoogleSans-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/GoogleSans-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/GoogleSans-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/GoogleSans-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+    {
+      path: '../assets/fonts/GoogleSans-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../assets/fonts/GoogleSans-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+  ],
 });
 
 export const metadata: Metadata = {
@@ -18,8 +49,8 @@ type RootLayoutProps = Readonly<PropsWithChildren>;
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
-      <body className={fontSans.className}>{children}</body>
+    <html lang="en" className={fontSans.variable}>
+      <body>{children}</body>
     </html>
   );
 }
--- landing/tailwind.config.ts
+++ landing/tailwind.config.ts
@@ -1,17 +1,18 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
+        mono: ['var(--font-mono)', ...defaultTheme.fontFamily.mono],
+        serif: ['var(--font-serif)', ...defaultTheme.fontFamily.sans],
       },
     },
   },
