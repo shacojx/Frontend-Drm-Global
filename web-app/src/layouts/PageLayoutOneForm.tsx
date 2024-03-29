d58389fac97facd862113f217035153eb1c33da4
luc.le <luc.le@enouvo.com>
2024-03-29 21:09:32 +0700

    change logo

--- web-app/src/layouts/PageLayoutOneForm.tsx
+++ web-app/src/layouts/PageLayoutOneForm.tsx
@@ -1,27 +1,26 @@
-import { PropsWithChildren } from "react";
-import { useNavigate } from "react-router-dom";
-import { FooterDefault } from "../components/base/footers";
-import { RoutePaths } from "src/constants/routerPaths";
-import IMAGE from "src/assets/images";
+import { PropsWithChildren } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { FooterDefault } from '../components/base/footers';
+import { RoutePaths } from 'src/constants/routerPaths';
+import IMAGE from 'src/assets/images';
 
 export function PageLayoutOneForm(props: PropsWithChildren<{}>) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   function handleClickLogo() {
-    navigate(RoutePaths.home)
+    navigate(RoutePaths.home);
   }
-  return <div
-    className="w-screen min-h-screen p-4 bg-cover flex flex-col"
-    style={{backgroundImage: `url(${IMAGE.backGround})`}}
-  >
-    <div className="flex flex-col items-center justify-center grow my-4">
-      <div className="w-full max-w-[400px]">
-        <img onClick={handleClickLogo} className="mb-8 cursor-pointer" src={IMAGE.logoFull} alt="logo_full"/>
-        <div className="flex flex-col gap-y-8 py-12 px-8 shadow-form rounded-3xl bg-[#ffffff]">
-          {props.children}
+  return (
+    <div
+      className="w-screen min-h-screen p-4 bg-cover flex flex-col"
+      style={{ backgroundImage: `url(${IMAGE.backGround})` }}
+    >
+      <div className="flex flex-col items-center justify-center grow my-4">
+        <div className="w-full max-w-[400px]">
+          <img onClick={handleClickLogo} className="mb-8 cursor-pointer h-20" src={IMAGE.logoFull} alt="logo_full" />
+          <div className="flex flex-col gap-y-8 py-12 px-8 shadow-form rounded-3xl bg-[#ffffff]">{props.children}</div>
         </div>
       </div>
+      <FooterDefault />
     </div>
-    <FooterDefault />
-  </div>
-
+  );
 }
