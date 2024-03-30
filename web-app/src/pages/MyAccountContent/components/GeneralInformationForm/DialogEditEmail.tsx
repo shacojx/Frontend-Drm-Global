cc28230933869b46a15270b46e28c886bb37b9bf
luc.le <luc.le@enouvo.com>
2024-03-30 16:41:50 +0700

    change susscess dialog change email text

--- web-app/src/pages/MyAccountContent/components/GeneralInformationForm/DialogEditEmail.tsx
+++ web-app/src/pages/MyAccountContent/components/GeneralInformationForm/DialogEditEmail.tsx
@@ -182,7 +182,7 @@ export const DialogEditEmail = ({ onClose, open }: DialogEditEmailProps) => {
       {isSuccess && (
         <DialogSuccessFullscreen
           title="Email changed successfully"
-          subTitle="Start your journey with us !"
+          subTitle="Email changed successfully"
           actionElement={
             <button
               className="rounded-lg bg-primary text-white font-semibold h-13 w-full mt-10"
